8dc514e3a184bf394a7c1d6c184074d467e749c1
Tapple214 <applegate21441@gmail.com>
2026-03-28 18:16:29 +0700

    parsers.py cleanup

--- src/example/components/parsers.py
+++ src/example/components/parsers.py
@@ -1,76 +1,8 @@
 from components.lexica import MyLexer
 from components.memory import Memory
 from sly import Parser
-
-# TODO: remove after completion
-# class MyParser(Parser):
-#     debugfile = 'parser.out'
-#     start = 'statement'
-#     # Get the token list from the lexer (required)
-#     tokens = MyLexer.tokens
-#     precedence = (
-#         ('left', "+", MINUS),
-#         ('left', TIMES, DIVIDE),
-#         ('right', UMINUS),
-#         )
-
-#     def __init__(self):
-#         self.memory:Memory = Memory()
-
-#     @_('NAME ASSIGN expr')
-#     def statement(self, p):
-#         var_name = p.NAME
-#         value = p.expr
-#         self.memory.set(variable_name=var_name,value=value, data_type=type(value))
-#         # Note that I did not return anything
-
-#     @_('expr')
-#     # S -> E
-#     def statement(self, p) -> int:
-#         return p.expr
-
-#     # The example with literals
-#     @_('expr "+" expr')
-#     # E -> E + E
-#     def expr(self, p):
-#         # You can refer to the token 2 ways
-#         # Way1: using array
-#         print(p[0], p[1], p[2])
-#         # Way2: using symbol name. 
-#         # Here, if you have more than one symbols with the same name
-#         # You have to indiciate the number at the end.
-#         return p.expr0 + p.expr1
-
-#     # The example with normal token
-#     @_('expr MINUS expr')
-#     def expr(self, p):
-#         print(p[0], p[1], p[2])
-#         return p.expr0 - p.expr1
-
-#     @_('expr TIMES expr')
-#     def expr(self, p):
-#         return p.expr0 * p.expr1
-
-#     @_('expr DIVIDE expr')
-#     def expr(self, p):
-#         return p.expr0 / p.expr1
-
-#     # https://sly.readthedocs.io/en/latest/sly.html#dealing-with-ambiguous-grammars
-#     # `%prec UMINUS` is the way to override the `precedence` of MINUS to UMINUS.
-#     @_('MINUS expr %prec UMINUS')
-#     def expr(self, p):
-#         return -p.expr
-
-#     @_('LPAREN expr RPAREN')
-#     def expr(self, p):
-#         return p.expr
-
-#     @_('NUMBER')
-#     def expr(self, p):
-#         return int(p.NUMBER)
-
-
 from components.ast.statement import Expression, Expression_logic, Expression_bool, Operations
+
 class ASTParser(Parser):
     debugfile = 'parser.out'
     start = 'statement'
@@ -107,10 +39,10 @@ class ASTParser(Parser):
         
 if __name__ == "__main__":
     lexer = MyLexer()
-    # parser = MyParser()
-    text = "t ∨ f ∧ t" # change to match
+    text = "t ∨ f ∧ t"
     memory = Memory()
     parser = ASTParser()
     result = parser.parse(lexer.tokenize(text))
+    
     print(result)
-    # print(memory)
+    print(memory)
